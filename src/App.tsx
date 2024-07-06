0e748f636388fd579402a43939c0e72935c6be0f
Ramon Alcala <43355033+i2bgilberto@users.noreply.github.com>
2024-07-05 23:40:50 -0500

    Starter Commt

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css';
+import "./App.css";
+import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return (
+    <>
+      <h1 className="text-center">Todo List</h1>
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
